69ccd52084afddbfa8e89eb4f0228e517c910eb7
michaelwong3049 <michaelwong3049@gmail.com>
2024-08-26 12:31:23 -0400

    Implement Gemini messages to frontend

--- app/components/UserBubble.js
+++ app/components/UserBubble.js
@@ -1,12 +1,27 @@
 import { Box, Typography } from "@mui/material";
+import { useState, useEffect } from 'react'
 import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
 
-export default function UserBubble() {
+export default function UserBubble({ message }) {
+    const [displayedText, setDisplayedText] = useState('');
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        if (currentIndex < message.length) {
+            const timer = setTimeout(() => {
+                setDisplayedText(prevText => prevText + message[currentIndex]);
+                setCurrentIndex(prevIndex => prevIndex + 1);
+            }, 30);
+
+            return () => clearTimeout(timer);
+        }
+    }, [currentIndex, message]);
+
     return(
         <Box display="flex" alignSelf="flex-end" alignItems="center" >
             <Box p={2} sx={{ backgroundColor: "gray", marginBottom: "20px", alignSelf: "flex-end", borderRadius: "25px" }} >
                 <Typography>
-                    Give me professors in the computer science department in Columbia Unversity.
+                    {displayedText}
                 </Typography>
             </Box>
             <SentimentSatisfiedAltIcon sx={{ marginLeft: "10px", color: "white" }} />
