9f2ff664edb33c902fc2a176517b7f66ee59fda9
gtomchuk2005 <gtomchuk2005@gmail.com>
2024-08-25 20:27:56 -0400

    Integrate RAG to query vector DB when generating user response

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,44 +1,72 @@
-import { model } from "@/gemini";
+import { embeddingModel, chatModel } from "@/gemini";
+import { Pinecone } from "@pinecone-database/pinecone";
 import { NextResponse } from "next/server";
 
 let conversationHistory = []
 
 export async function POST(req) {
-    
+
     const generationConfig = {
         temperature: 1,
         topP: 0.95,
         topK: 64,
-        maxOutputTokens: 8192,
+        maxOutputTokens: 200,
         responseMimeType: "text/plain",
-      };
+    };
     
-    const userinput = await req.text()
+    const userInput = await req.text()
+
+    const pc = new Pinecone({
+        apiKey: process.env.PINECONE_API_KEY
+    })
+
+    const index = pc.index('rag').namespace('ns1')
 
     async function run() {
+
         conversationHistory.push({
             role: 'user',
-            parts: [{text : userinput}]
-    });
+            parts: [{text : userInput}]
+        });
 
-    const chatSession = model.startChat({
-        history: conversationHistory,
-        generationConfig
-    });
+        const embedding = (await embeddingModel.embedContent(userInput)).embedding
+        console.log(userInput)
 
-    const result = await chatSession.sendMessage(userinput);
+        const results = await index.query({ 
+            topK: 3,
+            includeMetadata: true,
+            vector: embedding['values']
+        })
 
-    const aiResponse = await result.response.text();
-    console.log(aiResponse)
+        let resultString = 'Returned Results'
 
-    conversationHistory.push({
-        role: 'model',
-        parts: [{text: aiResponse }]
-    });
+        results.matches.forEach((match) => {
+            resultString+=`\n
+            Professor: ${match.id}
+            Subject: ${match.metadata.subject}
+            Stars: ${match.metadata.stars}
+            Review: ${match.metadata.review}
+            \n`
+        })
 
-    return aiResponse;
+        console.log(resultString)
 
+        const chatSession = chatModel.startChat({
+            history: conversationHistory,
+            generationConfig
+        }); 
+
+        //const result = await chatSession.sendMessage(userInput);
+        //const refinedResponse = result.response.text() + resultString;
+        
+        conversationHistory.push({
+            role: 'model',
+            parts: [{text: resultString }]
+        });
+
+        return resultString;
     }
+
     const message = await run();
 
     return new NextResponse(message)
