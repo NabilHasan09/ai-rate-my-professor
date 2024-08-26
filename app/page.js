6b0dad22632c5f924bf6843b5aff55385de93b52
michaelwong3049 <michaelwong3049@gmail.com>
2024-08-26 13:03:17 -0400

    Replace ExampleThree with a button link to chatroom

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Button, Link } from '@mui/material'
 import ExampleOne from './components/ExampleOne'
 import ExampleTwo from './components/ExampleTwo'
-import ExampleThree from './components/ExampleThree'
 
 export default function Home() {
   return (
@@ -14,11 +13,16 @@ export default function Home() {
 
       <ExampleTwo/>
 
-      <Box display="flex" justifyContent="center" alignItems="center" height="200px" width="100%">
-        <Box width="90%" height="1px" border="1px solid rgba(209, 209, 209, 0.5)"/>
+      <Box display="flex" justifyContent="center" alignItems="center" >
+        <Button variant="contained" component={Link} href="/professor-ai" sx={{ fontSize: "1.5em", backgroundColor: "rgba(191, 191, 191, 0.5)", border: "1px solid rgba(130, 130, 130)", transition: "transform 0.3s ease-in-out, background-color 0.3s ease-in-out",  '&:hover': { backgroundColor: "rgba(150,150,150)", transform: "scale(1.1)"} }}>
+          Talk To ProfessorAI...
+        </Button>
       </Box>
 
-      <ExampleThree/>
+      <Box display="flex" justifyContent="center" alignItems="center" height="200px" width="100%">
+        <Box width="90%" height="1px"/>
+      </Box>
+
     </Box>
   );
 }
