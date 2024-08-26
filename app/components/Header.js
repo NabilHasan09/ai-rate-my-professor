0b5574a8eff396b8c5ab8f3fdc34ad17bcf184e9
michaelwong3049 <michaelwong3049@gmail.com>
2024-08-26 13:02:44 -0400

    Change href link to upload-professor

--- app/components/Header.js
+++ app/components/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
                 </Button>
 
                 <Box sx={{ marginLeft: "auto" }}>
-                    <Button variant="outlined" component={Link} href="/upload_professor" sx={{ color: "gray", borderColor: "gray", marginRight: "25px", transition: "transform 0.2s ease-in-out" ,"&:hover": { borderColor: "gray", transform: "scale(1.2)" } }} >
+                    <Button variant="outlined" component={Link} href="/upload-professor" sx={{ color: "gray", borderColor: "gray", marginRight: "25px", transition: "transform 0.2s ease-in-out" ,"&:hover": { borderColor: "gray", transform: "scale(1.2)" } }} >
                         Upload Your Professor
                     </Button>
                     <Button variant="outlined" sx={{ color: "gray", borderColor: "gray", transition: "transform 0.2s ease-in-out" ,"&:hover": { borderColor: "gray", transform: "scale(1.2)" } }} >
